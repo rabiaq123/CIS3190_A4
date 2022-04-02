3f449e7ab35c47683504fa232a237415224238cb
Rabia Qureshi <rqureshi@uoguelph.ca>
2022-04-01 22:07:54 -0400

    Created all required program files

---
+++ Ada/calce.adb
@@ -0,0 +1,9 @@
+-- 1. Translate the Algol-60 procedure to C.
+-- 2. Add a wrapper main program which calls the procedure. 
+-- The main program should also:
+-- 2.1. Prompt the user for the number of significant digits to calculate.
+-- 2.2. Prompt the user for the name of the file in which to store the value of e calculated.
+-- 2.3. Call the subprogram, keepe().
+-- 3. Create a subprogram, keepe(), which saves the value of e calculated in
+-- an ASCII file. It takes as input the calculated value of e, and the filename specified by the user.
+
